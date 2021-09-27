f4a1d040aedf206ed9906c61b990c0fc674e19ab
Sébastien Lampazona <lampazona.sebastien@gmail.com>
2021-09-27 20:51:51 +0200

    Ajout du model de base

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+// == Import
+import Button from '@mui/material/Button';
+import './styles.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <h1>Composant : App</h1>
+    <Button variant="contained">Hello World</Button>
+  </div>
+);
+
+// == Export
+export default App;
